8bfe42fb0dc4a49513c671c6454edd12c040b7ee
Anthony Turinski <nodeathking@gmail.com>
2021-04-10 15:13:26 -0700

    hello world check

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import './App.css';
+import React from 'react';
 
-function App() {
-  return ()
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>City Explorer</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
